b8e5187b2ecb21ad8e69eca2146dc7c00325a056
TomSawyer1 <117282498+TomSawyer1@users.noreply.github.com>
2023-06-02 15:12:28 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def newsletter():
+    # Données de la newsletter
+    titre = "Ma Newsletter"
+    articles = [
+        {
+            'titre': "Article 1",
+            'contenu': "Ceci est le contenu de l'article 1."
+        },
+        {
+            'titre': "Article 2",
+            'contenu': "Ceci est le contenu de l'article 2."
+        },
+        {
+            'titre': "Article 3",
+            'contenu': "Ceci est le contenu de l'article 3."
+        }
+    ]
+
+    # Rendu du template HTML
+    return render_template('newsletter.html', titre=titre, articles=articles)
+
+if __name__ == '__main__':
+    app.run()
